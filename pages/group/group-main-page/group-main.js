5386bea54e2be84ce58eaa9befe3ef8fc7a9953a
Alibabayy <yiyangf2@uci.edu>
2024-05-03 17:25:08 -0700

    Your commit message

--- pages/group/group-main-page/group-main.js
+++ pages/group/group-main-page/group-main.js
@@ -31,3 +31,51 @@ document.getElementById("add-group-button").addEventListener('click', addGroup);
 // for (let group of document.getElementsByClassName('group_object')) {
 //     group.addEventListener('click', removeGroup(group.id));
 // }
+
+
+
+
+
+// code to let the created group appear on the group main page
+
+// const removeGroup = (id) => {
+//     document.getElementById(id).remove();
+//     i--;
+// }
+
+// for (let group of document.getElementsByClassName('group_object')) {
+//     group.addEventListener('click', removeGroup(group.id));
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const groupBoard = document.getElementById('group-container');
+
+//     const loadGroups = () => {
+//         const groups = JSON.parse(localStorage.getItem('groups')) || [];
+//         groups.forEach((group, index) => {
+//             addGroup(group.groupName, index);
+//         });
+//     };
+
+//     const addGroup = (groupName, index) => {
+//         const groupHTML = `<li id="group-${index}" class="group-object">
+//                               <a href='../group-manage-page/group-manage.html'>
+//                                   <h1>${groupName}</h1>
+//                               </a>
+//                            </li>`;
+//         groupBoard.insertAdjacentHTML('beforeend', groupHTML);
+//     };
+
+
+//     document.getElementById('add-group-button').addEventListener('click', () => {
+//         const groupNameInput = document.getElementById('group-name');
+//         const groupName = groupNameInput.value.trim();
+//         const groups = JSON.parse(localStorage.getItem('groups')) || [];
+//         groups.push({ groupName: groupName });
+//         localStorage.setItem('groups', JSON.stringify(groups));
+//         addGroup(groupName, groups.length - 1);
+//         groupNameInput.value = ''; 
+//     });
+
+//     loadGroups();
+// });
