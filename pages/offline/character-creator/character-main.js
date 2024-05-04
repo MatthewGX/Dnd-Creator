6a61ec059b59f6d1a043a1c43bcdec8bf2d46583
Matthew Mendoza <mamendo8@uci.edu>
2024-05-03 20:17:18 -0700

    Added Offline guest Pages

---
+++ pages/offline/character-creator/character-main.js
@@ -0,0 +1,18 @@
+function updateStr(value) {
+    document.getElementById('str-mod').innerText = Math.floor((value - 10) / 2);
+}
+function updateDex(value) {
+    document.getElementById('dex-mod').innerText = Math.floor((value - 10) / 2);
+}
+function updateCon(value) {
+    document.getElementById('con-mod').innerText = Math.floor((value - 10) / 2);
+}
+function updateInt(value) {
+    document.getElementById('int-mod').innerText = Math.floor((value - 10) / 2);
+}
+function updateWis(value) {
+    document.getElementById('wis-mod').innerText = Math.floor((value - 10) / 2);
+}
+function updateCha(value) {
+    document.getElementById('cha-mod').innerText = Math.floor((value - 10) / 2);
+}
---
+++ pages/offline/wiki/wiki-glossary.js
@@ -0,0 +1,38 @@
+let selectedTab = null;
+
+let tabs = ['classes', 'races', 'attributes', 'backgrounds', 'alignments'];
+
+let tables = {'classes': ['Bard', 'Cleric', 'Monk', 'Paladin', 'Barbarian', 'Druid', 'Fighter', 'Ranger', 'Rogue', 'Magician', 'Warlock', 'Artificer', 'Wizard'],
+                'attributes': ['Strength', 'Constitution', 'Dexterity', 'Intelligence', 'Wisdom', 'Charisma'],
+                'backgrounds': [],
+                'alignments': ['Lawful Good', 'Neutral Good', 'Chaotic Good', 'Lawful Neutral', 'True Neutral', 'Chaotic Neutral', 'Lawful Evil', 'Neutral Evil', 'Chaotic Evil'],
+                'races': ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Half-Orc', 'Human', 'Tiefling']}
+
+const selectActive = (tabID) => {
+    // console.log(tabID);
+    for (let tempID of tabs) {
+        let tab = document.getElementById(`${tempID}Tab`);
+        let screen = document.getElementById(tempID);
+
+        if (tempID === tabID) {
+            tab.className += 'active';
+            screen.className += 'active';
+        }
+        else {
+            tab.className = '';
+            screen.className = 'screen';
+        }
+    }
+}
+
+for (let tabID of tabs) {
+    let tab = document.getElementById(`${tabID}Tab`);
+    tab.addEventListener('click', () => {selectActive(tabID);});
+
+    let screen = document.getElementById(tabID);
+    for (let item of tables[tabID]) {
+        screen.getElementsByTagName('ol')[0].insertAdjacentHTML('beforeend', `<li>${item}</li>`);
+    }
+}
+
+
