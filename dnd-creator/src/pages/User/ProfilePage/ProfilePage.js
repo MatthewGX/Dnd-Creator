fb1161345147b4beb80ee512af089bcf9c7d97d1
ShenghuaHuo <94018805+ShenghuaHuo@users.noreply.github.com>
2024-05-15 23:30:20 -0700

    WIP Profile Implementing

--- dnd-creator/src/pages/User/ProfilePage/ProfilePage.js
+++ dnd-creator/src/pages/User/ProfilePage/ProfilePage.js
@@ -1,9 +1,32 @@
-// ProfilePage.js
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './ProfilePage.css';
 
 const ProfilePage = () => {
-    return (
-        <h1>PROFILE PAGE</h1>
-    );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [profileCreated, setProfileCreated] = useState('');
+
+  useEffect(() => {
+    setUsername('JohnDoe142');
+    setPassword('********');
+    setProfileCreated('4/14/2024');
+  }, []);
+
+  return (
+    <div className="profile-page">
+      <main>
+        <section id="profile">
+          <img src="/path/to/profile.png" alt="Profile Picture" />
+          <div>
+            <p>Username: <span id="username">{username}</span></p>
+            <p>Password: <span id="password">{password}</span></p>
+            <p>Profile Created: <span id="profileCreated">{profileCreated}</span></p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default ProfilePage;
