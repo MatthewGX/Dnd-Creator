3795ecb7d9c921a9ba0b1013053b2bbb14593a36
ShenghuaHuo <94018805+ShenghuaHuo@users.noreply.github.com>
2024-05-17 21:20:01 -0700

    Reset pwd implemented

--- dnd-creator/src/pages/default/login/ResetPasswordPage/ResetPasswordPage.js
+++ dnd-creator/src/pages/default/login/ResetPasswordPage/ResetPasswordPage.js
@@ -1,9 +1,57 @@
-// ResetPasswordPage.js
+import React, { useState } from 'react';
+import './ResetPasswordPage.css';
 
 const ResetPasswordPage = () => {
+  const [username, setUsername] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleResetPassword = () => {
+    const user = JSON.parse(localStorage.getItem(username));
+    if (user) {
+      user.password = newPassword;
+      localStorage.setItem(username, JSON.stringify(user));
+      setMessage('Password reset successful!');
+    } else {
+      setMessage('Username not found!');
+    }
+  };
+
   return (
-    <h1>RESET PAGE</h1>
+    <div className="reset-password-page">
+      <main>
+        <div className="reset-container">
+          <h1>Reset Password</h1>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <div className="input-box">
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+              <label htmlFor="username">Username</label>
+            </div>
+            <div className="input-box">
+              <input
+                type="password"
+                id="new-password"
+                name="new-password"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+                required
+              />
+              <label htmlFor="new-password">New Password</label>
+            </div>
+            {message && <p className="message">{message}</p>}
+            <button type="button" onClick={handleResetPassword}>Reset Password</button>
+          </form>
+        </div>
+      </main>
+    </div>
   );
-}
+};
 
 export default ResetPasswordPage;
