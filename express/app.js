2ef803ad1ba978b149f571e66ce4d3930955b194
Matthew Mendoza <mamendo8@uci.edu>
2024-06-07 16:25:11 -0700

    Updated mongoDB to be local

--- express/app.js
+++ express/app.js
@@ -1,21 +1,27 @@
 const express = require('express');
-const client = require('./db.config');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-client.db('testing').createCollection('test1');
+// Connect to local mongoDB
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true,
+    useUnifiedTopology: true });
+const db = mongoose.connection;
 
+db.on('error', console.error.bind(console, 'connection error:'));
+// Bind connection to open event (to log when connection is successful)
+db.once('open', () => {
+console.log('Connected to MongoDB');
+});
+
+
+// Create Express Application
 const testRoutes = require('./routes/test-routes');
 const userRoutes = require('./routes/user-routes');
 
 const app = express();
 const port = 4000;
 
-app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-
-// app.use(express.json());
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     console.log('test');
@@ -23,20 +29,22 @@ app.get('/', (req, res) => {
 });
 
 app.get('/test', async (req, res) => {
-    let collection = client.db('testing').collection('test1');
-    let results = await collection.find({})
-    .limit(50)
-    .toArray();
-    res.send(results).status(200);
+    res.status(200).send('Hello World!');
+
+    // let collection = client.db('testing').collection('test1');
+    // let results = await collection.find({})
+    // .limit(50)
+    // .toArray();
+    // res.send(results).status(200);
 })
 app.post('/testpost', async (req, res) => {
-    let collection = client.db('testing').collection('test1');
+    // let collection = client.db('testing').collection('test1');
     
-    let testJSON = {'name': 'Hello World', 'id': 12345, 'groups': []}
+    // let testJSON = {'name': 'Hello World', 'id': 12345, 'groups': []}
 
-    collection.insertOne(testJSON);
+    // collection.insertOne(testJSON);
 
-    res.send(testJSON).status(200);
+    res.status(200).send('TESTING POST');
 })
 
 // app.use('/test', testRoutes);
